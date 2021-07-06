6be7fc02200e59cc21bee4665e6e776cd340ffc3
ak3028 <ak3028@bath.ac.uk>
2021-07-06 17:16:34 +0100

    Added the model for Contact that is asociated with a business card

--- contactDetails/models.py
+++ contactDetails/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from uploadBusinessCard.models import BusinessCard, models
 
 # Create your models here.
+class Contact(models.Model):
+    contactName = models.CharField(max_length=300)
+    contactOrganization = models.CharField(max_length=200)
+    contactEmail = models.EmailField(max_length=100)
+    contactPhoneNumber = models.CharField(max_length=20)
+    contactCardInfo = models.CharField(max_length=1000)
+    models.ForeignKey(BusinessCard, on_delete=models.CASCADE)
