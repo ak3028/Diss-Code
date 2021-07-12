852f33693ebf55e638d9bf1eeadf01dcedda8932
ak3028 <ak3028@bath.ac.uk>
2021-07-12 11:11:14 +0100

    Enabled the contact list to be managed from the admin pannel.

--- uploadBusinessCard/admin.py
+++ uploadBusinessCard/admin.py
@@ -1,7 +1,9 @@
+from contactDetails.models import Contact
 from django.contrib import admin
 
 from .models import BusinessCard
 
 admin.site.register(BusinessCard)
+admin.site.register(Contact)
 
 # Register your models here.
